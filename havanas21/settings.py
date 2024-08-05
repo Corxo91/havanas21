2379f529ab4de863b23d42bfc0d5ab6b0d8d9ed4
Corxo91 <100447464+Corxo91@users.noreply.github.com>
2024-08-05 18:03:55 -0400

    Update settings.py
    database sql

--- havanas21/settings.py
+++ havanas21/settings.py
@@ -74,12 +74,8 @@ WSGI_APPLICATION = 'havanas21.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        "NAME": "hv21$default",
-        "USER": "hv21",
-        "PASSWORD": "havanas21_db",
-        "HOST": "hv21.mysql.pythonanywhere-services.com",
-        "PORT": "3306",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
